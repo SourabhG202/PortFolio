738d7a172a4f4ba359289574ecc3d42fad8ced05
SourabhG202 <sourabhghodki579@gmail.com>
2024-09-11 16:31:06 +0530

    sourabh

---
+++ arrays.js
@@ -0,0 +1,30 @@
+/*arr = [4,"golu",true,[1,3,2]];
+for(let i=0;i<arr.length;i++){
+    console.log(i,arr[i]);
+}
+//for of loop
+brr = [3,7,8,5,54,56];
+for (const ele of brr) {
+    console.log(ele);
+    
+}
+let s = "Sourabh";
+console.log(s.slice(2.6));
+*/
+/*let x = {
+    //x is the object
+    name: "sourabh",
+    age: 24,
+    percentage: 92.5;
+    'is married': false
+};
+for (const key in x) {
+    console.log(key,x[key]);
+*/
+/*let Fullname= prompt("Enter your full name: ");
+let Username= "@"+Fullname+Fullname.length;
+console.log(Username);
+*/
+function countVowels(str){
+    
+}
